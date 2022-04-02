bcd5dfb9a69c237a27fba19f2a3c31fe09f39fc3
Norman Herrera <norman.herrera@ymail.com>
2022-04-02 09:29:27 -0700

    linked the html to the script, made a city.js file to interact with city.html file

---
+++ city.js
@@ -0,0 +1,3 @@
+var resultsContainerEl = document.getElementById("resultsContainer")
+
+// might want to make a button to refer back home
--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-fetch('', {
-  // The browser fetches the resource from the remote server without first looking in the cache.
-  // The browser will then update the cache with the downloaded resource.
-  cache: 'reload',
-})
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
+// fetch('', {
+//   // The browser fetches the resource from the remote server without first looking in the cache.
+//   // The browser will then update the cache with the downloaded resource.
+//   cache: 'reload',
+// })
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     console.log(data);
+//   });
+
+  var searchBtnEl = document.getElementById("searchBtn");
+
+  searchBtnEl.addEventListener("click", getData)
+// script file works and its connected
+  function getData() {
+    console.log("connected");
+  }
+  
