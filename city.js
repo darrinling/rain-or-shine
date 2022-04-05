a3a933dc034aca947a1e5d0fdaa5579e3314bd8f
Norman Herrera <norman.herrera@ymail.com>
2022-04-05 14:48:03 -0700

    need to commit to merge

--- city.js
+++ city.js
@@ -1,3 +1,8 @@
 var resultsContainerEl = document.getElementById("resultsContainer")
 
 // might want to make a button to refer back home
+console.log('connected');
+
+function displayWeather() {
+    
+}
